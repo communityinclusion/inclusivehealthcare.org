1e5619380dd7e53e14a1406128fdde77d9605fb9
Jeff Coburn <jeff.coburn@gmail.com>
2018-12-06 14:56:55 -0500

    Initial commit

---
+++ jc/jc.js
@@ -0,0 +1,14 @@
+<!--//
+/*This Script allows people to enter by using a form that asks for a
+UserID and Password*/
+function pasuser(form) {
+if (form.userName.value=="ihc_user") { 
+if (form.inputPassword.value=="inclusive2013") {              
+window.location.href="womcuuelvo39kmd7740038cnkd9/index.html" 
+} else {
+alert("Invalid Password")
+}
+} else {  alert("Invalid UserID")
+}
+}
+//-->
